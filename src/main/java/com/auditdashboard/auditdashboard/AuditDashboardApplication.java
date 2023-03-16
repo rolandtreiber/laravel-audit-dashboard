b95c9f6e3c6f9a1eabaea477caee826e21485873
Roland Treiber <info@thecaringdeveloper.com>
2023-03-16 08:48:34 +0000

    =initial commit

---
+++ src/main/java/com/auditdashboard/auditdashboard/AuditDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.auditdashboard.auditdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auditdashboard/auditdashboard/AuditDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditdashboard.auditdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
